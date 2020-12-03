17d93216a8ecca246832d9e0764da1f1135a57e9
Fabio Eduardo urrego Sandoval <eduardo.urrego@gmail.com>
2020-12-02 19:56:12 -0500

    Long to Integer

--- Desktop/ArtCuratorSpringJPA-GUI/src/main/java/mintic/unal/art/repositorios/SerieRepositorio.java
+++ Desktop/ArtCuratorSpringJPA-GUI/src/main/java/mintic/unal/art/repositorios/SerieRepositorio.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Repository;
  * @author joag
  */
 @Repository
-public interface SerieRepositorio extends JpaRepository<Serie, Int>{
+public interface SerieRepositorio extends JpaRepository<Serie, Integer>{
 
    
     
